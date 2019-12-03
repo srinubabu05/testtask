794dc6ed94309fe938f5814d9bad016fc876be0b
Lab-1 <Lab-1@DESKTOP-UHM8TAB>
2019-12-03 14:41:38 +0530

    this is my test demo project

---
+++ MyProject/src/GetSetDemo.java
@@ -0,0 +1,22 @@
+
+public class GetSetDemo {
+String  MyBankName;
+int MyBankAccountNumber;
+public String getMyBankName() {
+	return MyBankName;
+}
+public void setMyBankName(String myBankName) {
+	MyBankName = myBankName;
+}
+public int getMyBankAccountNumber() {
+	return MyBankAccountNumber;
+}
+public void setMyBankAccountNumber(int myBankAccountNumber) {
+	MyBankAccountNumber = myBankAccountNumber;
+}
+
+}
+
+
+
+
